e51cc5fc5b80bdc3e163926829e8dd61dceb0afa
n8wb <vanish172@gmail.com>
2019-06-01 16:27:23 +0000

    small corrections

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
 {
     // ISSUE 1.
 	char c = 255; // This is can be signed or unsigned see -funsigned-char on gcc
-	cout << "This is unsigned on android and signed on linux" <<endl;
+	cout << "This is defaulted to unsigned on android and signed on linux for gcc" <<endl;
     printf("%d\n",c);
     // ISSUE 2. Constructors. See point.h
     point<example_type> p(1,2);
